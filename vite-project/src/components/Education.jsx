c5d73503dc84b6339e032fb7c9a05f182e1691f2
Nim-droid-droid <levkoz2222@outlook.com>
2023-07-31 23:26:52 +0100

    Added hover state

--- vite-project/src/components/Education.jsx
+++ vite-project/src/components/Education.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 export default function Education(){
+  // when hovering over section, show add btn & add Experience (user input) to page
+  const [hover, setHover] = useState();
+
   return(
     <>
         <h1>Education</h1>
