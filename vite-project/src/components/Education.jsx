820266199b8f2de7439b36f9b05e2d0d2952429c
Nim-droid-droid <levkoz2222@outlook.com>
2023-08-01 22:06:28 +0100

    Added display functionality to button so its visible when user hovers over section

--- vite-project/src/components/Education.jsx
+++ vite-project/src/components/Education.jsx
@@ -2,14 +2,24 @@ import { useState } from "react"
 
 export default function Education(){
   // when hovering over section, show add btn & add Experience (user input) to page
-  const [hover, setHover] = useState();
+  const [hover, setHover] = useState({display: 'none'});
 
   return(
     <>
         <h1>Education</h1>
         <hr />
         <form action="">
-          
+{/* Hidden Button in the box. Move mouse in the box */}
+        <div style={{border: '1px solid gray', width: 300, height: 300, padding: 10, margin: 100}}
+                 onMouseEnter={e => {
+                     setHover({display: 'block'});
+                 }}
+                 onMouseLeave={e => {
+                     setHover({display: 'none'})
+                 }}
+            >
+                <button style={hover}>Click</button>
+            </div>
         </form>
     </>
   )
